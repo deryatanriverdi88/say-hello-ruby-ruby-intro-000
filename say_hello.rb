2ece5331894e4bdffc9d2b8a3cf9b92ac7e24306
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-05-26 02:50:22 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
 def say_hello(name, language="Ruby")
-  puts "Hello #{name}, We heard you are a great #{language} programmer."
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
 
 say_hello ("","Ruby")
